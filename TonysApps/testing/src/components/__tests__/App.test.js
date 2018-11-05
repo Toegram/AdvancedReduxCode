7d2ce63e0d2ffc5531b04a2c827f612bf883b73a
Tony Turetsky <TTuretsky@gmail.com>
2018-11-04 19:31:03 -0500

    made a thing

---
+++ TonysApps/testing/src/components/__tests__/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
+
+let wrapped;
+
+//before every single test, it will run this function reassigning the 'wrapped' var
+beforeEach(() => {
+  wrapped = shallow(<App />)
+});
+
+
+it('shows a comment box', () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1)
+})
